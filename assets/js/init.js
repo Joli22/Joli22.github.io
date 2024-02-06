7b3dff0300f33fa34f505c235cbfb89a641bb05c
Marko Radisic <radisic@sda-solutions.de>
2024-02-06 21:20:48 +0100

    Add masonry

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,3 +1,10 @@
 Fancybox.bind()
 
-
+// init Masonry
+var $grid = $('.masonry').masonry({
+  horizontalOrder: true
+});
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry('layout');
+});
